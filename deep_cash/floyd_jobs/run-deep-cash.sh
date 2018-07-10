a6a06aa0bee2020fbea23cb107228686121e31b1
cosmicBboy <niels.bantilan@gmail.com>
2018-07-10 19:14:33 -0400

    adjust run-deep-cash experiment

--- deep_cash/floyd_jobs/run-deep-cash.sh
+++ deep_cash/floyd_jobs/run-deep-cash.sh
@@ -5,7 +5,7 @@ floyd run --env pytorch-0.3 $@ \
     --n_layers=3 \
     --beta=0.99 \
     --n_episodes=2000 \
-    --per_framework_time_limit=600 \
-    --per_framework_memory_limit=6144 \
+    --per_framework_time_limit=180 \
+    --per_framework_memory_limit=3077 \
     --logger=floyd \
     --fit_verbose 0"
