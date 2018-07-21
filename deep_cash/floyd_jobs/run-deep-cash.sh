80cab0acd8315c0aa57a3fa0515d2ebe76df8fe5
cosmicBboy <niels.bantilan@gmail.com>
2018-07-21 13:22:02 -0400

    add multi-experiment analysis

--- deep_cash/floyd_jobs/run-deep-cash.sh
+++ deep_cash/floyd_jobs/run-deep-cash.sh
@@ -5,7 +5,7 @@ floyd run --env pytorch-0.3 $@ \
     --n_layers=3 \
     --beta=0.9 \
     --n_episodes=1000 \
-    --n_iter=10 \
+    --n_iter=50 \
     --per_framework_time_limit=600 \
     --per_framework_memory_limit=10000 \
     --logger=floyd \
