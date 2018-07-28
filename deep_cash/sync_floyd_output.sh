55780c91952ff6d217d360703ec523711cfdffbc
cosmicBboy <niels.bantilan@gmail.com>
2018-07-28 12:43:18 -0400

    add sync floyd output bash script
    this script is a utility for syncing floydhub outputs
    to the local file system

---
+++ deep_cash/sync_floyd_output.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Syncs the outputs from floydhub
+# Usage:
+# `./sync_floyd_output.sh <job_number>`
+
+rm -rf floyd_outputs/$1
+mkdir -p floyd_outputs/$1
+floyd data clone nielsbantilan/projects/deep-cash/$1/output
+mv cash_controller_mlfs_trial_* floyd_outputs/$1
+mv controller_trial_* floyd_outputs/$1
+mv rnn_cash_controller_experiment.csv floyd_outputs/$1
