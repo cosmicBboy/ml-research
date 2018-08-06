f30ef21f8c660ca45c07ed4580f379143493738e
cosmicBboy <niels.bantilan@gmail.com>
2018-08-06 17:37:07 -0400

    [experiment] medium size architecture spec
    part of tuning the architecture size
    
    floydhub job:
    https://www.floydhub.com/nielsbantilan/projects/deep-cash/133

--- deep_cash/floyd_jobs/run-deep-cash.sh
+++ deep_cash/floyd_jobs/run-deep-cash.sh
@@ -1,14 +1,14 @@
-# experiment: error reward is negative, small neural network
+# experiment: error reward is negative, medium neural network
 floyd run --env pytorch-0.3 --cpu2 \
-    --message 'shallow-thin-nn0_negative_error_reward=-0.1' \
+    --message 'medium_nn0_negative_error_reward=-0.1' \
     ". ./.env && \
     python experiments/run_deep_cash.py \
     --output_fp=/output \
     --n_trials=3 \
-    --input_size=30 \
-    --hidden_size=30 \
-    --output_size=30 \
-    --n_layers=3 \
+    --input_size=60 \
+    --hidden_size=60 \
+    --output_size=60 \
+    --n_layers=6 \
     --dropout_rate=0.2 \
     --beta=0.9 \
     --with_baseline \
