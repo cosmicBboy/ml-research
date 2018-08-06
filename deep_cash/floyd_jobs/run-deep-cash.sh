e21a425fa2d86f8b9c1d26c181b8c9dfd7bdfe26
cosmicBboy <niels.bantilan@gmail.com>
2018-08-06 12:37:53 -0400

    [experiment] baseline for normalized rewards
    this experiment looks at how training stability might
    improve by normalizing the rewards (mean-centrering, std-
    rescaling). More precisely we do this for the advantage
    (reward - baseline).
    
    results assocated with floydhub job:
    https://www.floydhub.com/nielsbantilan/projects/deep-cash/129

--- deep_cash/floyd_jobs/run-deep-cash.sh
+++ deep_cash/floyd_jobs/run-deep-cash.sh
@@ -1,12 +1,16 @@
-floyd run --env pytorch-0.3 $@ \
+# experiment: train controller with normalized rewards
+floyd run --env pytorch-0.3 --cpu2 \
+    --message 'baseline_normalized_reward' \
     ". ./.env && \
     python experiments/run_deep_cash.py \
     --output_fp=/output \
-    --n_trials=5 \
+    --n_trials=3 \
     --n_layers=30 \
     --beta=0.9 \
+    --with_baseline \
     --multi_baseline \
-    --n_episodes=1000 \
+    --normalize_reward \
+    --n_episodes=500 \
     --n_iter=10 \
     --per_framework_time_limit=600 \
     --per_framework_memory_limit=10000 \
