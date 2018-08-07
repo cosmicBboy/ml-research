d02986167a0046775ca35c4a3564be12d9662b43
cosmicBboy <niels.bantilan@gmail.com>
2018-08-07 10:59:50 -0400

    [experiment] medium nn with few iterations
    in this experiment the episode batch size is only 5.
    we also reduce the `beta` parameter used to maintain
    an exponential mean of past rewards. job:
    
    https://www.floydhub.com/nielsbantilan/projects/deep-cash/141

--- deep_cash/floyd_jobs/run-deep-cash.sh
+++ deep_cash/floyd_jobs/run-deep-cash.sh
@@ -1,5 +1,6 @@
 # experiment: error reward is zero, medium neural network
-# with stricter memory and time limits
+# with stricter memory and time limits, lower beta (so exponential mean of
+# past rewards influence baseline more), and only 5 iterations per episode.
 floyd run --env pytorch-0.3 --cpu2 \
     --message 'medium_nn1_stricter_limits' \
     ". ./.env && \
@@ -11,12 +12,12 @@ floyd run --env pytorch-0.3 --cpu2 \
     --output_size=30 \
     --n_layers=3 \
     --dropout_rate=0.3 \
-    --beta=0.9 \
+    --beta=0.7 \
     --with_baseline \
     --multi_baseline \
     --normalize_reward \
-    --n_episodes=2000 \
-    --n_iter=50 \
+    --n_episodes=10000 \
+    --n_iter=5 \
     --learning_rate=0.003 \
     --error_reward=0 \
     --per_framework_time_limit=180 \
