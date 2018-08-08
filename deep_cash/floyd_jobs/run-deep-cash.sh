a5119c364609d2baf76a9c371c98fcaaf5db1572
cosmicBboy <niels.bantilan@gmail.com>
2018-08-08 10:17:36 -0400

    [experiment] larger error reward penalty
    looking at how larger error reward effects training,
    along with a stricter per MLF fitting time limit
    
    job:
    https://www.floydhub.com/nielsbantilan/projects/deep-cash/145

--- deep_cash/floyd_jobs/run-deep-cash.sh
+++ deep_cash/floyd_jobs/run-deep-cash.sh
@@ -2,7 +2,7 @@
 # with stricter memory and time limits, lower beta (so exponential mean of
 # past rewards influence baseline more), and 30 iterations per episode.
 floyd run --env pytorch-0.3 --cpu2 \
-    --message 'medium_nn1_stricter_limits' \
+    --message 'medium_nn1_stricter_limits_error_reward_-0.25' \
     ". ./.env && \
     python experiments/run_deep_cash.py \
     --output_fp=/output \
@@ -19,8 +19,8 @@ floyd run --env pytorch-0.3 --cpu2 \
     --n_episodes=500 \
     --n_iter=30 \
     --learning_rate=0.003 \
-    --error_reward=-0.05 \
-    --per_framework_time_limit=180 \
+    --error_reward=-0.25 \
+    --per_framework_time_limit=60 \
     --per_framework_memory_limit=5000 \
     --logger=floyd \
     --fit_verbose=0"
