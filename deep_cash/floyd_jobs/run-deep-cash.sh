bb8d48e3f05f1b9c8059f6777ce7509699508a4a
cosmicBboy <niels.bantilan@gmail.com>
2018-08-09 16:48:41 -0400

    [experiment] try a smaller neural network
    this one has 30 hidden units.
    
    job:
    https://www.floydhub.com/nielsbantilan/projects/deep-cash/153

--- deep_cash/floyd_jobs/run-deep-cash.sh
+++ deep_cash/floyd_jobs/run-deep-cash.sh
@@ -1,14 +1,14 @@
-# experiment: error reward is negative, medium neural network
+# experiment: error reward is negative, small neural network
 # with stricter memory and time limits, lower beta (so exponential mean of
 # past rewards influence baseline more), and 16 iterations per episode.
 floyd run --env pytorch-0.3 --cpu2 \
-    --message 'medium_nn1_stricter_limits_error_reward_0_batch_16' \
+    --message 'small_nn0_stricter_limits_error_reward_0_batch_16' \
     ". ./.env && \
     python experiments/run_deep_cash.py \
     --output_fp=/output \
     --n_trials=6 \
     --input_size=30 \
-    --hidden_size=60 \
+    --hidden_size=30 \
     --output_size=30 \
     --n_layers=3 \
     --dropout_rate=0.3 \
@@ -16,7 +16,7 @@ floyd run --env pytorch-0.3 --cpu2 \
     --with_baseline \
     --multi_baseline \
     --normalize_reward \
-    --n_episodes=100 \
+    --n_episodes=200 \
     --n_iter=16 \
     --learning_rate=0.003 \
     --error_reward=-0.05 \
