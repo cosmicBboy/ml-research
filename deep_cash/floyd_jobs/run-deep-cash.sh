53ca91d5beefeae027b3f8d428182322428621b0
cosmicBboy <niels.bantilan@gmail.com>
2018-08-09 11:04:51 -0400

    [experiment] try out smaller batch sizes
    experiment in the same vein as 5c988af, but
    with a batch size of 32
    
    job:
    https://www.floydhub.com/nielsbantilan/projects/deep-cash/149

--- deep_cash/floyd_jobs/run-deep-cash.sh
+++ deep_cash/floyd_jobs/run-deep-cash.sh
@@ -1,8 +1,8 @@
 # experiment: error reward is negative, medium neural network
 # with stricter memory and time limits, lower beta (so exponential mean of
-# past rewards influence baseline more), and 64 iterations per episode.
+# past rewards influence baseline more), and 32 iterations per episode.
 floyd run --env pytorch-0.3 --cpu2 \
-    --message 'medium_nn1_stricter_limits_error_reward_-0.05_batch_64' \
+    --message 'medium_nn1_stricter_limits_error_reward_-0.05_batch_32' \
     ". ./.env && \
     python experiments/run_deep_cash.py \
     --output_fp=/output \
@@ -17,7 +17,7 @@ floyd run --env pytorch-0.3 --cpu2 \
     --multi_baseline \
     --normalize_reward \
     --n_episodes=1000 \
-    --n_iter=64 \
+    --n_iter=32 \
     --learning_rate=0.003 \
     --error_reward=-0.05 \
     --per_framework_time_limit=180 \
