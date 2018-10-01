82e3d5105bf8b4067f7c160fd41e94e8dab09c72
cosmicBboy <niels.bantilan@gmail.com>
2018-09-30 21:15:22 -0400

    remove unnecessary function in openml api

--- deep_cash/tests/test_openml_api.py
+++ deep_cash/tests/test_openml_api.py
@@ -3,14 +3,6 @@
 from deep_cash.data_sourcers import openml_api
 
 
-def test_parse_dataset():
-    datasets = [openml_api.parse_clf_dataset(d)
-                for d in openml_api.get_datasets()]
-    assert datasets[0]["dataset_name"] == "anneal"
-    assert datasets[0]["data"].shape == (898, 38)  # 898 obs, 38 features
-    assert datasets[0]["target"].shape == (898, )  # 898 obs, one class
-
-
 def test_list_clf_datasets():
     dataset_metadata = openml_api.list_clf_datasets(n_results=10)
     assert len(dataset_metadata) == 10
