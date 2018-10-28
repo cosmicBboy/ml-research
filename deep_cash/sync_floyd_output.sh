7e0d7a91946598df8ece7d3d693490b56bacc3ed
cosmicBboy <niels.bantilan@gmail.com>
2018-10-28 15:09:59 -0400

    sync logs from floyd

--- deep_cash/sync_floyd_output.sh
+++ deep_cash/sync_floyd_output.sh
@@ -10,3 +10,4 @@ floyd data clone nielsbantilan/projects/deep-cash/$1/output
 mv cash_controller_mlfs_trial_* floyd_outputs/$1
 mv controller_trial_* floyd_outputs/$1
 mv rnn_cash_controller_experiment.csv floyd_outputs/$1
+mv fit_predict_error_logs.log floyd_outputs/$1
